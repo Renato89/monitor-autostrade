6ba0ae1758e9bca968e87cee72cd72b9407be96f
Renato89 <r.fabiani@outlook.it>
2022-12-03 18:33:39 +0000

    fix variabili da file di configurazione

--- scripts/start-superset.sh
+++ scripts/start-superset.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+source ../config/path.config
 cd $SUPERSET_PATH
 
 docker-compose -f docker-compose-non-dev.yml pull
--- scripts/start-zookeeper.sh
+++ scripts/start-zookeeper.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+source ../config/path.config
 $KAFKA_PATH/bin/zookeeper-server-start.sh $KAFKA_PATH/config/zookeeper.properties
