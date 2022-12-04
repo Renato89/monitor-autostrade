69e87f42607f2e5a7ce4d9fc77db664ec5e807b6
Renato89 <r.fabiani@outlook.it>
2022-12-04 18:57:03 +0000

    correzione ultimi-avvistamenti: in uscita anche il timestamp in entrata e quello in uscita

--- src/last_seen.py
+++ src/last_seen.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.types import IntegerType, StringType, StructType, StructField
+from pyspark.sql.types import IntegerType, DoubleType, StringType, StructType, StructField
 from pyspark.sql.functions import (
     concat,
     count,
@@ -9,11 +9,14 @@ from pyspark.sql.functions import (
     count,
     max,
     first,
-    struct
+    last,
+    struct,
+    window
 )
 
 spark = SparkSession.builder.appName("LastSeen").getOrCreate()
 
+spark.sparkContext.setLogLevel('WARN')
 
 # Stream
 dfstream = (
@@ -35,31 +38,31 @@ dfstream = (
 
 # Immette i tratti autostradali
 tratti = [
-    (1, 27, 9),
-    (2, 9, 26),
-    (3, 26, 10),
-    (4, 10, 18),
-    (5, 18, 23),
-    (6, 23, 15),
-    (7, 15, 5),
-    (8, 5, 8),
-    (9, 8, 3),
-    (10, 3, 13),
-    (11, 22, 1),
-    (12, 1, 12),
-    (13, 12, 25),
-    (14, 25, 20),
-    (15, 20, 2),
-    (16, 2, 16),
-    (17, 16, 4),
-    (18, 4, 21),
+    (27, 9, 8.48),
+    (9, 26, 17.42),
+    (26, 10, 6.0),
+    (10, 18, 12.3),
+    (18, 23, 14.0),
+    (23, 15, 17.6),
+    (15, 5, 7.7),
+    (5, 8, 10.9),
+    (8, 3, 6.9),
+    (3, 13, 9.8),
+    (22, 1, 10.6),
+    (1, 12, 10.9),
+    (12, 25, 7.7),
+    (25, 20, 17.7),
+    (20, 2, 13.8),
+    (2, 16, 14.1),
+    (16, 4, 14.0),
+    (4, 21, 25.7),
 ]
 
 tratti_schema = StructType(
     [
-        StructField("tratto", IntegerType()),
         StructField("ingresso", IntegerType()),
         StructField("uscita", IntegerType()),
+        StructField("lunghezza", DoubleType())
     ]
 )
 
@@ -70,29 +73,25 @@ df_tratti = spark.createDataFrame(data=tratti, schema=tratti_schema).cache()
 #                       solo l'ultimo timestamp e tratto autostradale in cui
 #                       si trova
 ultimi_avvistamenti = (
-    dfstream.join(df_tratti, dfstream.varco == df_tratti.ingresso, "inner")
-    .groupBy("targa")
+    dfstream.join(df_tratti, (dfstream.varco == df_tratti.ingresso) | (dfstream.varco == df_tratti.uscita), 'left')
+    # .groupBy(window(dfstream.timestamp,  "10 minutes", "5 minutes") , \
+    .groupBy(\
+        'targa', 'ingresso', 'uscita') \
     .agg(
-        max(struct("timestamp", "tratto", "ingresso")).alias("max"),
-        count(lit(1)).alias("avvistamenti"),
-        first("nazione").alias("nazione"),
-    )
-    .select(
-        "targa",
-        "max.timestamp",
-        "max.tratto",
-        "max.ingresso",
-        "avvistamenti",
-        "nazione",
+        first('timestamp').alias('partenza'), 
+        last('timestamp').alias('arrivo'),
+        count('timestamp').alias('avvistamenti')
     )
 )
 
 
 
 def foreach_batch_id(df, epoch_id):
-    df.withColumn("key", lit(str(epoch_id))).write.format("kafka").option(
-        "kafka.bootstrap.servers", "localhost:9092"
-    ).option("topic", "ultimi-avvistamenti").save()
+    df.withColumn("key", lit(str(epoch_id))) \
+        .write \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "localhost:9092") \
+        .option("topic", "ultimi-avvistamenti").save()
     pass
 
 
@@ -101,42 +100,27 @@ print("\n\n\nStarting...\n\n\n")
 query = (
     ultimi_avvistamenti.select(
         concat(
-            "targa",
-            lit(","),
-            "timestamp",
-            lit(","),
-            "tratto",
-            lit(","),
-            "ingresso",
-            lit(","),
-            "avvistamenti",
-            lit(","),
-            "nazione",
+            "targa", lit(","),
+            "ingresso", lit(","),
+            "uscita", lit(","),
+            "partenza", lit(","),
+            "arrivo", lit(","),
+            "avvistamenti"
         ).alias("value")
     )
     .writeStream.foreachBatch(foreach_batch_id)
-    .outputMode("complete")
+    .outputMode("update")
     .start()
 )
 
 # Test
-""" query_console = ultimi_avvistamenti\
-    .select(concat(
-        "targa", lit(","),
-        "timestamp", lit(","),
-        "tratto", lit(","),
-        "ingresso", lit(","),
-        "avvistamenti", lit(","),
-        "nazione").alias("value")) \
-    .writeStream \
-    .foreachBatch(foreach_batch_id) \
-    .format("console") \
-    .option("numRows", 200) \
-    .outputMode("complete") \
-    .start()
- """
+# query = ultimi_avvistamenti\
+#     .writeStream \
+#     .format("console") \
+#     .option("numRows", 200) \
+#     .outputMode("complete") \
+#     .start()
 
-""" query_console.stop()
- """
+
 
 query.awaitTermination()
