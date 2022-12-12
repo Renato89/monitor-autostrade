7350e8c6b6538223c844c47c46c3157c29d23837
Renato89 <r.fabiani@outlook.it>
2022-12-12 22:14:35 +0000

    si cancella il topic prima dell'esecuzione perché se non rimangono in memoria i record vecchi

--- scripts/vehicle-count.sh
+++ scripts/vehicle-count.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+source config/path.config
+$KAFKA_PATH/bin/kafka-topics.sh --delete --topic conteggio-per-tratto --bootstrap-server localhost:9092
 spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2 src/vehicle_count.py
